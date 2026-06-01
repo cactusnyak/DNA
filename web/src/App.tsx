f717cb77c0f595baa9ffc9e2f9b9844b55663072
Fedor Shevchenko <cactusnyak@gmail.com>
2026-06-01 20:55:35 +0300

    feat(): implement routing and create HomePage, Header, and Footer components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,6 @@
-function App() {
-  return (
-    <>
-      
-    </>
-  )
-}
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/router';
 
-export default App
+export default function App() {
+  return <RouterProvider router={router} />;
+}
---
+++ web/src/pages/home/HomePage.tsx
@@ -0,0 +1,16 @@
+import { Footer } from '../../widgets/Footer/Footer';
+import { Header } from '../../widgets/Header/Header';
+
+export function HomePage() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h1>Главная</h1>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
